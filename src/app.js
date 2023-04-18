300b2ae79a4aa9a8c6d18c2bf0beec864940391e
rizwana337 <120568097+rizwana337@users.noreply.github.com>
2023-04-18 19:41:01 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,22 +12,26 @@ const server = http.createServer((req, res) => {
       const value1 = obj.num1;
       const value2 = obj.num2;
 
-      if (value1 <= 0) {
+      // Check if num1 is a positive integer
+      if (!Number.isInteger(value1) || value1 <= 0) {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ error: 'num1 must be a positive integer' }));
+        res.end(JSON.stringify({ error: "num1 must be a positive integer" }));
         return;
       }
 
-      if (value2 < 0) {
+      // Check if num2 is a non-negative integer
+      if (!Number.isInteger(value2) || value2 < 0) {
         res.statusCode = 400;
         res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ error: 'num2 must be a non-negative integer' }));
+        res.end(JSON.stringify({ error: "num2 must be a non-negative integer" }));
         return;
       }
 
+      // Calculate the exponential result
       const result = Math.pow(value1, value2);
 
+      // Return the result in the response body
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
       res.end(`The result is ${result}`);
